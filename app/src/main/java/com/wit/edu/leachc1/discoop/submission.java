cb1ae41c12bc9dfd19321bf2e759e55d0e33a836
kanners <kanners@wit.edu>
2018-06-15 01:17:45 -0400

    edited some dimensions and added a toast popup for submit button

--- app/src/main/java/com/wit/edu/leachc1/discoop/submission.java
+++ app/src/main/java/com/wit/edu/leachc1/discoop/submission.java
@@ -1,8 +1,19 @@
 package com.wit.edu.leachc1.discoop;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class submission extends AppCompatActivity {
 
@@ -10,5 +21,15 @@ public class submission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    public void toastSubmitMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void toastSubmit(View v) {
+        toastSubmitMsg("Your discount has been submitted");
     }
 }
