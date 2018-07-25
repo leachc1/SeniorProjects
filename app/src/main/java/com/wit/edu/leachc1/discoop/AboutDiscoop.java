88c0161d7b36d7507ab03764848647739eb88cd0
Crissy Leach <leachc1@wit.edu>
2018-07-25 18:15:48 -0400

    About page

---
+++ app/src/main/java/com/wit/edu/leachc1/discoop/AboutDiscoop.java
@@ -0,0 +1,21 @@
+package com.wit.edu.leachc1.discoop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AboutDiscoop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_discoop);
+    }
+
+    public void returnButton(View v) {
+                finish();
+                }
+}
